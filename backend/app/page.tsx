c322e0d359dcac18e2ca4eaefa658b9a9caef42d
vishal <vg2556519@gmail.com>
2026-06-07 00:22:50 +0530

    docker-minor

--- backend/app/page.tsx
+++ backend/app/page.tsx
@@ -1,6 +1,8 @@
 import { redirect } from "next/navigation";
 import { getFrontendUrl } from "@/lib/env";
 
+export const dynamic = "force-dynamic";
+
 export default function Home() {
   redirect(getFrontendUrl());
 }
